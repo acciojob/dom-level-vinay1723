8cf292e8a3bc75ab4ac56755b67975bf6944662a
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-19 12:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 //your JS code here. If required.
-let element = documement.getElementById('level');
-
-
-
+let element = document.getElementById('level');
 	let level = 0;
     while(element.parentElement) {
 	        level++;
 	        element = element.parentElement;
            }
-    alert(`The level of the element is: ${level}`);
+    alert(`The level of the element is: ${level+1}`);
 
 
